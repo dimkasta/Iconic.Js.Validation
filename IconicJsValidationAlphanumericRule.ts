3d644da47e4b579ed4f4329c1d8034e6c6b33a5c
dimkasta <dimkasta@yahoo.gr>
2017-11-03 15:16:39 +0200

    Delete IconicJsValidationAlphanumericRule.ts

--- IconicJsValidationAlphanumericRule.ts
+++
@@ -1,9 +0,0 @@
-class IconicJsValidationAlphanumericRule extends IconicJsValidationRule {
-    public constructor(required: boolean) {
-        super();
-        if(required) {
-            this.addRule("required", true, IconicJsValidationMessage.REQUIRED);
-        }
-        this.addRule("pattern", IconicJsValidationPattern.ALPHANUMERIC, IconicJsValidationMessage.ALPHANUMERIC);
-    }
-}
