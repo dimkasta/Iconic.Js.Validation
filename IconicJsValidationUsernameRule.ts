501b830928113f6ef2c246ea5f0d8e93f1b85783
dimkasta <dimkasta@yahoo.gr>
2017-11-03 15:19:04 +0200

    Delete IconicJsValidationUsernameRule.ts

--- IconicJsValidationUsernameRule.ts
+++
@@ -1,8 +0,0 @@
-class IconicJsValidationUsernameRule extends IconicJsValidationRule {
-    public constructor(length: number) {
-        super();
-        this.addRule("required", true, IconicJsValidationMessage.REQUIRED);
-        this.addRule("minlength", length, IconicJsValidationMessage.MINLENGTH);
-        this.addRule("pattern", IconicJsValidationPattern.ALPHANUMERIC, IconicJsValidationMessage.ALPHANUMERIC);
-    }
-}
