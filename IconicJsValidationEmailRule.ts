e5f637ed4f02f2202c3131f93f2a5019ae5acd81
dimkasta <dimkasta@yahoo.gr>
2017-11-03 15:16:46 +0200

    Delete IconicJsValidationEmailRule.ts

--- IconicJsValidationEmailRule.ts
+++
@@ -1,7 +0,0 @@
-class IconicJsValidationEmailRule extends IconicJsValidationRule {
-    public constructor() {
-        super();
-        this.addRule("required", true, IconicJsValidationMessage.REQUIRED);
-        this.addRule("email", true, IconicJsValidationMessage.EMAIL);
-    }
-}
