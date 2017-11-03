3213482facc25836ddfb78422418de0b759b828a
dimkasta <dimkasta@yahoo.gr>
2017-11-03 15:17:10 +0200

    Delete IconicJsValidationPasswordRule.ts

--- IconicJsValidationPasswordRule.ts
+++
@@ -1,7 +0,0 @@
-class IconicJsValidationPasswordRule extends IconicJsValidationRule {
-    public constructor(length: number) {
-        super();
-        this.addRule("required", true, IconicJsValidationMessage.REQUIRED);
-        this.addRule("minlength", length, IconicJsValidationMessage.MINLENGTH);
-    }
-}
