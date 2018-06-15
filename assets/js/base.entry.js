5ffe185d5b48fac48087a6c0a604bf7ff7288ac6
dimkasta <dimkasta@yahoo.gr>
2018-06-15 15:25:43 +0300

    Delete base.entry.js

--- assets/js/base.entry.js
+++
@@ -1,4 +0,0 @@
-window.$=$;
-import IconicFinanceCalculator from "./IconicFinanceCalculator/IconicFinanceCalculator"
-window.FinanceCalculator = new IconicFinanceCalculator();
-console.info("Loaded Finance Calculator");
